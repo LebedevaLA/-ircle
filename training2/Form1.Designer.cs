9eeec761244f97c9efed36790c72cba86aa1058c
LebedevaLA <148869645+LebedevaLA@users.noreply.github.com>
2024-10-24 23:57:44 +0300

    Add files via upload

---
+++ training2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace training2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(541, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 164);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(46, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 351);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(626, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вверх";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(541, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Влево";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(705, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Вправо";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button4.Location = new System.Drawing.Point(626, 374);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Вниз";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button5.Location = new System.Drawing.Point(485, 452);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 52);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Нарисовать поле";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Lime;
+            this.button6.Location = new System.Drawing.Point(732, 459);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 45);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Старт";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 516);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ training2/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace training2
+{
+    public partial class Form1 : Form
+    {
+        private int n;
+        private int m;
+        private List<List<bool>> space;
+        private Point curr;
+        private int h;
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.BackColor = Color.White;
+            panel1.Height = 240;
+            panel1.Width = 240;
+            n = 8;
+            m = 8;
+            h = 0;
+            space = new List<List<bool>>();
+            for(int i = 0; i < n; i++)
+            {
+                List<bool> tmp=new List<bool>();
+                for(int j = 0; j < m; j++)
+                {
+                    tmp.Add(false);   
+                }
+                space.Add(tmp);
+            }
+            listBox1.Enabled = false;
+            button6.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            curr.X = -1;
+            curr.Y = -1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics Draw_space = e.Graphics;
+            SolidBrush brush = new SolidBrush(Color.Gray);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    if (space[i][j] == true)
+                    {
+                        Draw_space.FillRectangle(brush, i * 30, j * 30, 30, 30);
+                    }
+                }
+            }
+            if (curr.X != -1 && curr.Y != -1 && timer1.Enabled==false)
+            {
+                SolidBrush brush1 = new SolidBrush(Color.Red);
+                Draw_space.FillEllipse(brush1, curr.X * 30, curr.Y * 30, 30, 30);
+            }
+            if (timer1.Enabled == true && listBox1.Items.Count!=0)
+            {
+                
+               
+                if (listBox1.Items[h].ToString() == "Вверх")
+                {
+                    curr.Y -= 1;
+                }
+                if (listBox1.Items[h].ToString() == "Вниз")
+                {
+                    curr.Y += 1;
+                }
+                if (listBox1.Items[h].ToString() == "Вправо")
+                {
+                    curr.X += 1;
+                }
+                if (listBox1.Items[h].ToString() == "Влево")
+                {
+                    curr.X -= 1;
+                }
+                h++;
+                if (space[curr.X][curr.Y] == true)
+                {
+                    timer1.Stop();
+                    h = 0;
+                    Draw_space.Clear(Color.White);
+                    MessageBox.Show("Game Over");
+                    //Draw_space.Clear(Color.White);
+                }
+                if (h == listBox1.Items.Count)
+                {
+                    timer1.Stop();
+                    button6.Enabled = false;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    //space.Clear();
+                    h = 0;
+                }
+                SolidBrush brush1 = new SolidBrush(Color.Red);
+                Draw_space.FillEllipse(brush1, curr.X * 30, curr.Y * 30, 30, 30);
+                
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = "Вверх";
+            listBox1.Items.Add(text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = "Вниз";
+            listBox1.Items.Add(text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = "Вправо";
+            listBox1.Items.Add(text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = "Влево";
+            listBox1.Items.Add(text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int count = rnd.Next(5,12);
+            int i;
+            int j;
+            int c=rnd.Next(0,n);
+            int d=rnd.Next(0,m);
+            for(int k = 0; k < count; k++)
+            {
+                i = rnd.Next(0, n);
+                j = rnd.Next(0, m);
+                space[i][j] = true;
+            }
+            while (space[c][d] == true)
+            {
+                c = rnd.Next(0, 8);
+                d = rnd.Next(0, 8);
+            }
+            curr = new Point(c, d);
+            listBox1.Enabled = true;
+            button6.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            panel1.Refresh();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //start
+            listBox1.Enabled = false;
+            timer1.Start(); 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            panel1.Refresh();
+        }
+    }
+}
+
